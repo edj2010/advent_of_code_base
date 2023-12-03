8e1615f65ea0b4b55117efafa50fa64792243d06
Evan Johnson <evan.durston.johnson@gmail.com>
2023-12-03 13:04:06 -0500

    fixed build errors + unused code warnings

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@
 
 mod day_template;
 
+#[allow(dead_code)]
 const YEAR: &str = "2022";
+
+#[allow(dead_code)]
 const COOKIE_PATH: &str = "../session.cookie";
+
+#[allow(dead_code)]
 const INPUT_CACHE: &str = "input";
 
 fn main() {
